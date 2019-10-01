0c77a1d2843d312d5f392f6a84ce61c3c617d76a
Peter Swanson <topazoo42@gmail.com>
2019-10-01 00:51:03 -0700

    Refactored socket into standalone class

--- backend/views.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-from flask import render_template, send_from_directory
-
-def render_index():
-    print('HERE')
-    return render_template('index.html')
-
-
--- static/js/sockets.js
+++ static/js/sockets.js
@@ -3,7 +3,6 @@
 angular.module('Sockets_Library', ['ngWebSocket'])
 .controller('Weight_Socket', ['$scope', '$websocket', function($scope, $websocket) {
     $websocket('ws://localhost:5001').onMessage(function(message) {
-        data = JSON.parse(message.data);
-        $scope.weight = data.weight;
+        $scope.weight = JSON.parse(message.data).data;
     });
 }]);
